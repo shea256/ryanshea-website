18f0b8fc9ede396f839cf6db09ff47f3a86c3ff6
Ryan Shea <ryan@shea.io>
2021-11-10 17:36:49 -0500

    Update gen-rss.js

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,9 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml'
+    title: 'Ryan Shea',
+    site_url: 'https://shea.io',
+    feed_url: 'https://shea.io/feed.xml'
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
